e6d4d53f3b9df854ff1cf98c695f57770ca94679
IsanchezV <a01664141@tec.mx>
2024-10-05 17:22:52 -0600

    Update the code

---
+++ Agricultural/src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+    es: 'Español',
+    en: 'Ingles',
+    fr : 'Frances'
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'nav.home': 'Inicio',
+        'routes.home': 'Inicio',
+    },
+    en: {
+        'nav.home': 'Home',
+        'routes.home': 'Home',
+    },
+    fr: {
+        'nav.home': 'Accueil',
+        'routes.home': 'Accueil',
+    },
+} as const;
